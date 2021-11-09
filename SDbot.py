2a06ed93ce02353698d7437d0fb6b6f5c774ea95
Sᴀᴅᴇᴡ Jᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-09 12:43:06 +0530

    Update SDbot.py

--- SDbot.py
+++ SDbot.py
@@ -77,7 +77,7 @@ async def home(client, message):
         InlineKeyboardButton('Source Code 💾', url='https://github.com/Sadew451/SDTelegraphBot')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
-  await SDBots.send_message(
+  await SDBots.send_sticker(STICKER)
         chat_id=message.chat.id,
         text="""👋 Hey there,
         
